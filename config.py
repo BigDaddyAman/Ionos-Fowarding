3cdfc5c443d3311299b5485225e241bd9c7ea47f
BigDaddyAman <139612136+BigDaddyAman@users.noreply.github.com>
2025-03-30 08:11:15 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 API_ID = 20370320  # Your Telegram API ID
 API_HASH = "35ee047c1a13d07951f4ce68cf1be144"  # Your Telegram API Hash
 SESSION_NAME = "your_session"  # Your Pyrogram session name
-SOURCE_CHANNEL = -1002570648989 # Replace with your source channel ID
+SOURCE_CHANNEL = -1002647276011 # Replace with your source channel ID
 DEST_CHANNEL = -1002570648989  # Replace with your destination channel ID
